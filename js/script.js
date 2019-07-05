7ce29f266b5e251254af84ba1559b58a378acd57
Raddy <r.angelov@live.co.uk>
2019-07-06 00:34:59 +0100

    IE11 fixes

--- js/script.js
+++ js/script.js
@@ -41,86 +41,25 @@ jQuery( document ).ready(function() {
 
  
 
-    jQuery(function($){
+// IE/Edge fallback for responsive images while 'object-fit' property is not supported
+function ObjectFitIt() {
+  jQuery('img.objFit').each(function(){
+    var imgSrc = jQuery(this).attr('src');
+    var fitType = 'cover';
 
-        // Set all variables to be used in scope
-        var frame,
-            metaBox = $('#meta-box-id.postbox'), // Your meta box id here
-            addImgLink = metaBox.find('.upload-custom-img'),
-            delImgLink = metaBox.find( '.delete-custom-img'),
-            imgContainer = metaBox.find( '.custom-img-container'),
-            imgIdInput = metaBox.find( '.custom-img-id' );
-        
-        // ADD IMAGE LINK
-        addImgLink.on( 'click', function( event ){
-          
-          event.preventDefault();
-          
-          // If the media frame already exists, reopen it.
-          if ( frame ) {
-            frame.open();
-            return;
-          }
-          
-          // Create a new media frame
-          frame = wp.media({
-            title: 'Select or Upload Media Of Your Chosen Persuasion',
-            button: {
-              text: 'Use this media'
-            },
-            multiple: false  // Set to true to allow multiple files to be selected
-          });
-      
-          
-          // When an image is selected in the media frame...
-          frame.on( 'select', function() {
-            
-            // Get media attachment details from the frame state
-            var attachment = frame.state().get('selection').first().toJSON();
-      
-            // Send the attachment URL to our custom image input field.
-            imgContainer.append( '<img src="'+attachment.url+'" alt="" style="max-width:100%;"/>' );
-      
-            // Send the attachment id to our hidden input
-            imgIdInput.val( attachment.id );
-      
-            // Hide the add image link
-            addImgLink.addClass( 'hidden' );
-      
-            // Unhide the remove image link
-            delImgLink.removeClass( 'hidden' );
-          });
-      
-          // Finally, open the modal on click
-          frame.open();
-        });
-        
-        
-        // DELETE IMAGE LINK
-        delImgLink.on( 'click', function( event ){
-      
-          event.preventDefault();
-      
-          // Clear out the preview image
-          imgContainer.html( '' );
-      
-          // Un-hide the add image link
-          addImgLink.removeClass( 'hidden' );
-      
-          // Hide the delete image link
-          delImgLink.addClass( 'hidden' );
-      
-          // Delete the image id from the hidden input
-          imgIdInput.val( '' );
-      
-        });
-      
-      });
+    if(jQuery(this).data('fit-type')) {
+      fitType = jQuery(this).data('fit-type');
+    }
 
+    jQuery(this).parent().css({ 'background' : 'transparent url("'+imgSrc+'") no-repeat center center/'+fitType, });
+    jQuery('img.objFit').css('display','none'); });
+    }
 
+  if('objectFit' in document.documentElement.style === false) {
+  ObjectFitIt();
+}
 
-
-
+ 
 
 });
 
